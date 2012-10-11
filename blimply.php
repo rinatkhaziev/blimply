acf1406ec243cbaab5e6e79580c2b801476c20a2
Rinat Khaziev <rinat.khaziev@gmail.com>
2012-10-11 17:46:21 -0500

    Add support for sounds and urls in push

--- blimply.php
+++ blimply.php
@@ -87,6 +87,7 @@ class Blimply {
 			return;
 
 		$this->options = get_option( 'urban_airship' );
+		$this->sounds = get_option( 'blimply_sounds' );
 		$this->airships[ $this->options['blimply_name'] ] = new Airship( $this->options['blimply_app_key'], $this->options['blimply_app_secret'] );
 		// Pass the reference to convenience var
 		// We don't use multiple Airships yet.
@@ -153,7 +154,7 @@ class Blimply {
 
 		if ( 1 == $_POST['blimply_push'] ) {
 			$alert = !empty( $_POST['blimply_push_alert'] ) ? esc_attr( $_POST['blimply_push_alert'] ) : esc_attr( $_POST['post_title'] );
-			$this->_send_broadcast_or_push( $alert, $_POST['blimply_push_tag'] );
+			$this->_send_broadcast_or_push( $alert, $_POST['blimply_push_tag'], get_permalink( $post_id ) );
 			update_post_meta( $post_id, 'blimply_push_sent', true );
 		}
 	}
@@ -179,14 +180,24 @@ class Blimply {
 	 * @param string  $tag
 	 *
 	 */
-	function _send_broadcast_or_push( $alert, $tag ) {
+	function _send_broadcast_or_push( $alert, $tag, $url = false ) {
 		// Strip escape slashes, otherwise double escaping would happen
 		$alert = stripcslashes( $alert );
 		$payload = array( 'aps' => array( 'alert' => $alert, 'badge' => '+1' ) );
+		// Add a URL if any, to be handled by apps
+		if ( $url ) 
+			$payload['aps']['url'] = $url;
+
+
 		if ( $tag === 'broadcast' ) {
 			$response =  $this->request( $this->airship, 'broadcast', $payload );
 		} else {
 			// Adding tags field to payload, no problem.
+			if ( isset( $this->sounds["blimply_sound_{$tag}"] ) && !empty( $this->sounds["blimply_sound_{$tag}"] ) ) 
+				$payload['aps']['sound'] = $this->sounds["blimply_sound_{$tag}"];
+			else
+				$payload['aps']['sound'] = 'default';
+
 			$payload['tags'] = array( $tag );
 			$response = $this->request( $this->airship, 'push', $payload );
 		}
